a576b88f54b69655c374361a13d77a514782480e
Engedi <emese.engedi@accenture.com>
2020-03-18 10:29:38 +0100

    feat: added header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-    <h1>notes.</h1>
+    <h1 class="page-title ml-3">notes.</h1>
     <router-view />
+    <section class="flex justify-center">
+      <p class="m-auto">Made with Vue</p>
+    </section>
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Cedarville+Cursive&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  .page-title {
+    font-family: "Cedarville Cursive", cursive;
+    font-size: 3rem;
+  }
 }
 </style>
