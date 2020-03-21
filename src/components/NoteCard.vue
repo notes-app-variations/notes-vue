7649812e09ad17de3f8f7495e1e9ce44684a00c2
Engedi <emese.engedi@accenture.com>
2020-03-21 16:35:54 +0100

    :sparkles: started home page with note cards

--- src/components/NoteCard.vue
+++ src/components/NoteCard.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="note-card"></div>
+  <div class="note-card rounded shadow-md">
+    <h2>{{ note.title }}</h2>
+    <p>{{ note.body }}</p>
+    <span
+      class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
+      >{{ note.category }}</span
+    >
+    <p class="text-xs italic text-gray-400">{{ note.createdAt }}</p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,7 +15,7 @@ import { Component, Prop, Vue } from "vue-property-decorator"
 
 @Component
 export default class NoteCard extends Vue {
-  @Prop() private msg!: string
+  @Prop() private note!: string
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <note-card msg="Welcome to Your Vue.js App" />
+  <div class="home h-full w-4/5 m-auto mt-8">
+    <router-link to="/note" class="btn-main bg-red-500 hover:bg-red-400"
+      >Create new</router-link
+    >
+    <note-card v-for="n in notes" :key="n._id" :note="n" />
   </div>
 </template>
 
@@ -13,6 +15,28 @@ export default {
   name: "Home",
   components: {
     NoteCard
+  },
+  data() {
+    return {
+      notes: [],
+      alert: ""
+    }
+  },
+  async mounted() {
+    const user = JSON.parse(localStorage.getItem("user"))
+    console.log(user)
+    const uid = user._id
+    const result = await fetch(`http://localhost:5000/api/notes/user/${uid}`, {
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    })
+    if (result.ok) {
+      this.notes = await result.json()
+      console.log(this.notes)
+    } else {
+      this.alert = await result.json()
+    }
   }
 }
 </script>
